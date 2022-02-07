e27779c4ab106ff88175170d3e41fd65e84b2f09
0x A.K <0x.bejaxer@gmail.com>
2022-02-07 22:13:19 +1000

    fix: remove the Global on landmark

--- src/landmark.ts
+++ src/landmark.ts
@@ -1,21 +1,8 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts";
 import { LandmarkMinted } from "./entities/PIXLandmark/PIXLandmark";
-import {
-  Global,
-  LandmarkType,
-  Landmark,
-  LandmarkAccount,
-} from "./entities/schema";
+import { LandmarkType, Landmark, LandmarkAccount } from "./entities/schema";
 
 export function handleLandmarkMinted(event: LandmarkMinted): void {
-  let entity = Global.load("landmarks");
-  if (entity == null) {
-    entity = new Global("landmarks");
-    entity.value = new BigInt(0);
-  }
-  entity.value = entity.value.plus(BigInt.fromI32(1));
-  entity.save();
-
   let landmark = new Landmark(event.params.tokenId.toString());
   createLandmarkAccount(event.params.account);
   landmark.account = event.params.account.toHexString();
@@ -23,7 +10,6 @@ export function handleLandmarkMinted(event: LandmarkMinted): void {
   landmark.category = BigInt.fromI32(event.params.category);
   createLandmarkType(event.params.landmarkType);
   landmark.landmarkType = event.params.landmarkType.toString();
-
   landmark.save();
 
   let account = LandmarkAccount.load(event.params.account.toHexString());
